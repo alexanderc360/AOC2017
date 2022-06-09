b5025b11ebd2558050d66c0af06b8eb6db7f4847
Alex Cohen <cohenah@clarkson.edu>
2022-06-09 16:55:16 -0400

    finished day 8

--- sources/day8.cpp
+++ sources/day8.cpp
@@ -9,6 +9,8 @@
 
 using namespace std;
 
+map<string, int> editRegister(map<string, int> regs, string step[7]);
+
 void dayEight() {
 
     ifstream input("day8.txt");
@@ -16,6 +18,7 @@ void dayEight() {
 
     map<string, int> regs;
     string step[7];
+    int max = 0;
 
     while (getline(input, str)) {
         step[0] = str.substr(0, str.find(' '));
@@ -39,29 +42,69 @@ void dayEight() {
         step[6] = str.substr(0, str.find(' '));
         str = str.substr(str.find(' ') + 1);
 
+        for (const auto &i: regs)
+            if (i.second > max)
+                max = i.second;
 
-        for (const auto &i: step)
-            cout << i << " ";
-        cout << endl;
-        regs[step[0]] = 0;
+        regs = editRegister(regs, step);
     }
+
+
+
+    cout << max;
 }
 
-
-map<string, int> part1(map<string, int> regs, string step[7]) {
+map<string, int> editRegister(map<string, int> regs, string step[7]) {
     string focus = step[0], dependent = step[4], compare = step[5];
     bool increase = step[1] == "inc";
     int val = stoi(step[2]), compareNum = stoi(step[6]);
 
     if (regs.count(focus) == 0)
-        
+        regs[focus] = 0;
+    if (regs.count(dependent) == 0)
+        regs[dependent] = 0;
 
 
-    if(increase){
-        if (compare == ">")
-            if
-    }else{
-
+    if (increase) {
+        if (compare == ">") {
+            if (regs[dependent] > compareNum)
+                regs[focus] += val;
+        } else if (compare == "<") {
+            if (regs[dependent] < compareNum)
+                regs[focus] += val;
+        } else if (compare == ">=") {
+            if (regs[dependent] >= compareNum)
+                regs[focus] += val;
+        } else if (compare == "<=") {
+            if (regs[dependent] <= compareNum)
+                regs[focus] += val;
+        } else if (compare == "==") {
+            if (regs[dependent] == compareNum)
+                regs[focus] += val;
+        } else if (compare == "!=") {
+            if (regs[dependent] != compareNum)
+                regs[focus] += val;
+        }
+    } else {
+        if (compare == ">") {
+            if (regs[dependent] > compareNum)
+                regs[focus] -= val;
+        } else if (compare == "<") {
+            if (regs[dependent] < compareNum)
+                regs[focus] -= val;
+        } else if (compare == ">=") {
+            if (regs[dependent] >= compareNum)
+                regs[focus] -= val;
+        } else if (compare == "<=") {
+            if (regs[dependent] <= compareNum)
+                regs[focus] -= val;
+        } else if (compare == "==") {
+            if (regs[dependent] == compareNum)
+                regs[focus] -= val;
+        } else if (compare == "!=") {
+            if (regs[dependent] != compareNum)
+                regs[focus] -= val;
+        }
     }
 
     return regs;
